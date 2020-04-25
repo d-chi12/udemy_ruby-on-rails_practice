3cb221ffaf98108dce58e910e256b775e792150b
USER <user@USERnoMacBook-ea.local>
2020-04-25 10:19:01 +0900

    method

---
+++ method.rb
@@ -0,0 +1,18 @@
+#Hello World!と出力するメソッドを記述
+#メソッドを実行
+#メソッド名は、hello_worldとする
+
+# def hello_world
+#     puts 'Hello World!'
+# end
+
+# hello_world
+
+#2つの数字の足し算を行い、その値を返すメソッドを記述
+#メソッドはaddとする
+
+def add(x, y)
+    x + y
+end
+
+puts add(10, 1)
